9de6d7655c9843e4c43cf3a9c85f3087943256b6
Alexandra597 <sasham597@gmail.com>
2014-10-25 21:58:16 +0300

    Add details view controller

---
+++ NewYorkTimes/ABCDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABCDetailsViewController.h
+//  NewYorkTimes
+//
+//  Created by Admin on 25.10.14.
+//  Copyright (c) 2014 mmf-3-iOs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCDetailsViewController : UIViewController
+
+@end
